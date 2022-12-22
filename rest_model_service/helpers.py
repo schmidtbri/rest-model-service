2b221150db8435e068a4e5662e70fcf797e1e141
Brian Schmidt <6666331+schmidtbri@users.noreply.github.com>
2022-12-21 19:20:19 -0500

    adding exception logging to initialization

--- rest_model_service/helpers.py
+++ rest_model_service/helpers.py
@@ -109,6 +109,8 @@ def set_service_status(future: Future) -> None:
         health_status_manager.set_health_status(HealthStatus.NOT_HEALTHY)
         health_status_manager.set_readiness_status(ReadinessStatus.REFUSING_TRAFFIC)
         health_status_manager.set_startup_status(StartupStatus.STARTED)
+        exception = future.exception()
+        logger.error("Exception raised in service initialization.", exc_info=exception)
     else:
         health_status_manager.set_health_status(HealthStatus.HEALTHY)
         health_status_manager.set_readiness_status(ReadinessStatus.ACCEPTING_TRAFFIC)
